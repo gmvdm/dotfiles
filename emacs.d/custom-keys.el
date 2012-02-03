5f05cb324e413e6a12234bd15011b79bd35f18e0
Geoff Wilson <gmwils@spotify.com>
2012-02-03 10:34:29 +0100

    Bind C-t to textmate-goto-file

--- emacs.d/custom-keys.el
+++ emacs.d/custom-keys.el
@@ -11,12 +11,14 @@
   (global-set-key (kbd "\C-r") 'isearch-backward-regexp)
   (global-set-key (kbd "C-M-s") 'isearch-forward)
   (global-set-key (kbd "C-M-r") 'isearch-backward)
-  
+
   ;; M-S-6 is awkward
   (global-set-key (kbd "C-c q") 'join-line)
 
   ;; Help should search more than just commands
   (global-set-key (kbd "C-h a") 'apropos)
+
+  (global-set-key (kbd "C-t") 'textmate-goto-file)
 
   (global-set-key (kbd "C-x m") 'magit-status)
   (global-set-key (kbd "M-s")   'fixup-whitespace)
