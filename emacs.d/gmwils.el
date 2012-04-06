8f0df9b6062edb6bb45d1f51636947e93a12813d
Geoff Wilson <gmwils@gmail.com>
2012-04-06 11:43:18 +0200

    Add dired-x support

--- emacs.d/gmwils.el
+++ emacs.d/gmwils.el
@@ -9,6 +9,19 @@
 (add-to-list 'auto-mode-alist '("\\.doc$" . doc-mode))
 (add-to-list 'auto-mode-alist '("\\.asciidoc$" . doc-mode))
 
+;; Advanced dired
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")
+            ;; Set dired-x global variables here.  For example:
+            ;; (setq dired-guess-shell-gnutar "gtar")
+            ;; (setq dired-x-hands-off-my-keys nil)
+            ))
+(add-hook 'dired-mode-hook
+          (lambda ()
+            ;; Set dired-x buffer-local variables here.  For example:
+            ;; (dired-omit-mode 1)
+            ))
 
 (require 'textmate)
 (textmate-mode)
