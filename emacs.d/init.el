d09e28a4e773291951d24b27d4544b2dea502f1a
Geoff Wilson <gmwils@spotify.com>
2012-06-15 16:27:01 +0200

    Add scala mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -34,7 +34,7 @@
 		      haskell-mode go-mode
                       js2-mode
 		      ;; ruby-mode inf-ruby
-                      php-mode
+		      scala-mode php-mode
                       workgroups
                       smart-tab protobuf-mode
                       marmalade oddmuse scpaste))
