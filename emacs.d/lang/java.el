325ea1ed7d39e914abd21dc7a96aee88cc802073
Geoff Wilson <gmwils@spotify.com>
2013-07-31 07:19:07 +0200

    Kill tabs for spaces.

--- emacs.d/lang/java.el
+++ emacs.d/lang/java.el
@@ -4,7 +4,7 @@
 (add-hook 'java-mode-hook (lambda ()
                             (setq c-basic-offset 2
                                   tab-width 2
-                                  indent-tabs-mode t)))
+                                  indent-tabs-mode nil)))
 
 
 (provide 'java)
