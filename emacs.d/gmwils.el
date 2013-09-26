38e4b612db089a34d510a1a12674a25dd6eaecd9
Geoff Wilson <gmwils@spotify.com>
2013-09-26 21:53:02 +0200

    Minor emacs fixes

--- emacs.d/gmwils.el
+++ emacs.d/gmwils.el
@@ -27,19 +27,7 @@
 (setq wg-prefix-key (kbd "C-c w"))
 (wg-load "~/.emacs.d/workgroups")
 
-;; Advanced dired
-(add-hook 'dired-load-hook
-          (lambda ()
-            (load "dired-x")
-            ;; Set dired-x global variables here.  For example:
-            ;; (setq dired-guess-shell-gnutar "gtar")
-            ;; (setq dired-x-hands-off-my-keys nil)
-            ))
-(add-hook 'dired-mode-hook
-          (lambda ()
-            ;; Set dired-x buffer-local variables here.  For example:
-            ;; (dired-omit-mode 1)
-            ))
+(require 'dired-x)
 
 (defun conditionally-enable-paredit-mode ()
   "Enable `paredit-mode' in the minibuffer, during `eval-expression'."
