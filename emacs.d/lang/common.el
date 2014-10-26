e81d06726b6fbd2adcbf428b4c9ac81c623bbd8e
Geoff van der Meer <gmwils@spotify.com>
2014-10-26 23:15:59 +0100

    Fix up git commit mode.

--- emacs.d/lang/common.el
+++ emacs.d/lang/common.el
@@ -40,7 +40,6 @@
 
 ;; Associate modes with file extensions
 
-(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
