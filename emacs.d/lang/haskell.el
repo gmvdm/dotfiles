edad4eb56060805bad7b0847d4dd34273b20d4ac
Geoff van der Meer <gmwils@spotify.com>
2014-12-31 17:52:12 +0100

    Updated haskell config.

--- emacs.d/lang/haskell.el
+++
@@ -1,7 +0,0 @@
-;;;; haskell.el - settings for haskell programming
-
-
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
-(provide 'haskell)
