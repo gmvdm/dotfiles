b5dd9492c74d32bd5a59f8a92314fa7f6dace1c4
Geoff van der Meer <gvdm@sophon.home>
2022-09-10 22:39:37 +0200

    Update for modern Emacs

---
+++ emacs.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type 'cabal-repl)
+ '(haskell-tags-on-save t)
+ '(package-selected-packages '(smart-tab use-package diminish)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,18 +5,31 @@
 ;; in Emacs lisp embedded in *one* literate Org-mode file.
 
 ;;; Code:
-(when (version< emacs-version "25")
+(when (version< emacs-version "28")
   (warn "This configuration needs Emacs trunk, but this is %s!" emacs-version))
 
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+	        use-package-expand-minimally t))
+
 ;; This sets up the load path so that we can override it
-(package-initialize nil)
+;; (package-initialize nil)
 
 ;; Override the packages with the git version of Org and other packages
-(add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/lisp/")
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/org-mode/lisp/")
 
 ;; Load the rest of the packages
-(package-initialize t)
-(setq package-enable-at-startup nil)
+;; (package-initialize t)
+;; (setq package-enable-at-startup nil)
 
 (require 'org)
 (org-babel-load-file "~/.emacs.d/gmwils.org")
